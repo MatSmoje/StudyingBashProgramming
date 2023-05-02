e0e131145aa1665022c479af2e1ce60129031a2d
matias smoje <matias.smoje@gmail.com>
2023-05-02 12:13:15 -0400

    Darts complete :)

--- Darts.sh
+++ Darts.sh
@@ -4,47 +4,80 @@
 
 main()
 {
-	local numOne=$1
-	local numTwo=$2
-	absoluteValue;
-	getMax;
+	        
+        local totalArgs
+        local args
+        totalArgs=$#
+        args=$@
 
-	if (( $(echo "$max<=1"| bc -l) )); then
-		echo "10"
-	elif (( $(echo "$max<=5" | bc -l) )); then
-		echo "5"
-	elif (( $(echo "$max<=10" | bc -l) )); then
-		echo "1"
-	else
-		echo "0"
-	fi
+        ArgsValidator;
 
+        max=$(echo "sqrt($1^2+$2^2)" | bc -l)
+
+        if (( $(echo "$max<=1"| bc -l) )); then
+                echo "10"
+        elif (( $(echo "$max<=5" | bc -l) )); then
+                echo "5"
+        elif (( $(echo "$max<=10" | bc -l) )); then
+                echo "1"
+        else
+                echo "0"
+        fi
 }
-getMax()
+ArgsValidator()
 {
-	max=$numOne
-	if [[ $numOne>$numTwo  ]]; then
-		max=$numOne
-	else
-		max=$numTwo
-	fi
-}
-absoluteValue()
-{
-	if (( $(echo "$numOne>=0"| bc -l) )); then 
-		numOne=$numOne
-	else 
-		numOne=$(echo "$numOne*-1" | bc)
-       	fi
+	 if [[ $totalArgs -ne 2 ]]; then
+            echo "Error"
+            exit 1
+        fi
 
-
-	if (( $(echo "$numTwo>=0"| bc -l) ));then 
-		numTwo=$numTwo
-	else 
-		numOne=$(echo "$numTwo*-1" | bc)
-       	fi
+        for argz in $args; do
+            if [[ ! "$argz" =~ ^-?[0-9]+([.][0-9]+)?$ ]]; then
+                echo "Error - ${argz}"
+                exit 1
+            fi
+        done
 }
 
-
-#getMax "$@"
 main "$@"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- readme.md
+++ readme.md
@@ -10,5 +10,15 @@ I am currently doing some exercises from the webpage: https://exercism.org/
 ## Useful 
 - $RANDOM: Return random number. If you want to get random between a range, you can do $(($RANDOM%26)), where 26 is the upper range +1.
 
+## Notes
+
+- Darts.sh  
+I believe this excercise was not too easy. I had to solve how to validate my input arguments generating a regex patron. 
+    - Concepts
+        - Conditional
+        - Regex
+        - Functions
+        - bc
+        - errors 
 
 
