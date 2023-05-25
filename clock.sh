c51914cc34f178f7b8005b3911ef5fa248e9da13
matias smoje <matias.smoje@gmail.com>
2023-05-24 23:06:04 -0400

    new

--- clock.sh
+++ clock.sh
@@ -3,30 +3,30 @@
 
 main()
 {
-    hour=0
+    hour="$1"
     min="$2"
 
+    if [[ "$1" -lt 0 && "$2" -lt 0 ]]; then
+        optHour=$((24+($1%24)-($2/60)))
+        optMin=$((60+($2%60)))
+    elif [[ "$1" -ge 0 && "$2" -ge 0 ]]; then
+        optHour=$((($1%24)+($2/60)))
+        optMin=$(($2%60))
+    elif [[ "$1" -ge 0 && "$2" -lt 0 ]]; then
+        optHour=$((($1+((-70+$2)/60))%24))
+        optMin=$((60+($2%60)))
+    elif [[ "$1" -le 0 && "$2" -ge 0 ]]; then
+        optHour=$((($1+($2/60)%24)))
+        optMin=$(($2%60))
+    fi
     
-
-    i=0
-    while [ "$i" -le "$1" ]; do
-        i=$(($i+1))
-
-        
-        if [[ "$i" -lt "1"  ]]; then
-            echo "test"
-
-        if [[ "$i" -gt "1"  ]]; then
-            hour=$(($hour+1))
-        fi
-
-        if [[ "$hour" -eq 24 ]];then
-            hour=0
-        fi
-
-    done
-
-    echo "hora: $hour"
+    if [[ "$optHour" -lt 10 ]]; then
+        optHour="0$optHour"
+    fi
+    if [[ "$optMin" -lt 10 ]]; then
+        optMin="0$optMin"
+    fi
+    echo "$optHour:$optMin"
 }
 
 main "$@"
